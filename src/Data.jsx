660b06c6f99aa45a3c0d1b41715e130751076817
Heidy Primo <heidyprimo10@hotmail.com>
2024-03-14 17:43:41 -0500

    installing libraries and creating components and to configure the redux envrioment to the app

---
+++ src/Data.jsx
@@ -0,0 +1,17 @@
+export const userList= [
+    {
+        name: "Heidy Primo", 
+        email: "heidy@email.com",
+        id: 1
+    },
+    {
+        name: "Camila Villarreal", 
+        email: "camila@email.com",
+        id: 2
+    },
+    {
+        name: "Helena Diaz", 
+        email: "helena@email.com",
+        id: 3
+    },
+]
