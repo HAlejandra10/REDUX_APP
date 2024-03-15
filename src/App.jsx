b4d45a0bcd695b50d6da86cf2c4fc7df28ae4b46
Heidy Primo <heidyprimo10@hotmail.com>
2024-03-15 15:55:31 -0500

    add Create component and routing in the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Home from './Home'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Create from './Create'
 
 const App = () => {
   return (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />}></Route>
-      <Route path="/" element={<Home />}></Route>
+      <Route path="/" element={<Create />}></Route>
     </Routes>
   </BrowserRouter>
   )
