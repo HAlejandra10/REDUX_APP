f691b84960b6b5594fbf1f0d14ba1d37f3c1d309
Heidy Primo <heidyprimo10@hotmail.com>
2024-03-15 16:22:49 -0500

    change into Home file button to Link of the reat-router-dom

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSelector } from "react-redux"
+import { Link } from 'react-router-dom'
 
 const Home = () => {
     const users = useSelector((state) => state.users)
@@ -8,7 +9,7 @@ const Home = () => {
   return ( 
     <div className="container">
         <h2>REDUX APP WITH JSON SERVER</h2>
-        <button className='btn btn-success my-3'>Create +</button>
+        <Link to="./create" className='btn btn-success my-3'>Create +</Link>
         <table className='table'>
             <thead>
                 <tr>
