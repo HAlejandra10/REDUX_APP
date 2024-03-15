ece22accd82b4e52e00f9d19c3e81f1901133168
Heidy Primo <heidyprimo10@hotmail.com>
2024-03-15 16:52:22 -0500

    adding details as title and placeholer to Create component

--- src/Create.jsx
+++ src/Create.jsx
@@ -4,14 +4,15 @@ const Create = () => {
   return (
     <div className='d-flex w-100 vh-100 justify-content-center align-items-center'>
       <div className="w-50 border bg-secondary text-white p-5">
+        <h3>Add New User</h3>
         <form>
           <div>
             <label htmlFor="name">Name:</label>
-            <input type="text" className="form-control" id="name" />
+            <input type="text" className="form-control" id="name"  placeholder='enter name'/>
           </div>
           <div>
             <label htmlFor="email">Email:</label>
-            <input type="email" className="form-control" id="email" />
+            <input type="email" className="form-control" id="email" placeholder='eter email' />
           </div> <br />
           <button className="btn btn-info">Submit</button>
         </form>
