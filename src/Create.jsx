1d4a01509028309af701ead49102c71e0d41d774
Heidy Primo <heidyprimo10@hotmail.com>
2024-03-17 14:05:16 -0500

    adding onSubmit event method on the form

--- src/Create.jsx
+++ src/Create.jsx
@@ -4,11 +4,18 @@ import { useState } from 'react'
 const Create = () => {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
+
+
+  const handleSumit=(event) => {
+    event.preventDefault();
+    //console.log(name, email)
+  }
+
   return (
     <div className='d-flex w-100 vh-100 justify-content-center align-items-center'>
       <div className="w-50 border bg-secondary text-white p-5">
         <h3>Add New User</h3>
-        <form>
+        <form onSubmit={handleSumit}>
           <div>
             <label htmlFor="name">Name:</label>
             <input type="text" className="form-control" id="name"  placeholder='Enter Name' onChange={e => setName(e.target.value)} />
